57f711b6851e41f2dadefb9c0059732ddad4d4ea
royalcrab win <hoge@mplpl.com>
2021-02-23 12:02:19 +0900

    modify script

--- scripts/arrange_leds.py
+++ scripts/arrange_leds.py
@@ -8,6 +8,15 @@
 import pcbnew
 import re
 
+# name    : "SK6812MINI", "C0603", "etc"
+# x       : length of the column
+# y       : length of the raw
+# pitch_mm: pitch between each pair of parts
+# dir     : 0/2 virtical, 1/3 horizontal, 2,3 reverse in odd raws or columns
+# ox      : offset x
+# oy      : offset y
+# visible : visibility of reference silk
+
 def arrange_leds(name,x,y,pitch_mm,dir=0,rot=0,ox=0,oy=0,visible=False):
     board = pcbnew.GetBoard()
     mods = board.GetModules()
@@ -22,11 +31,15 @@ def arrange_leds(name,x,y,pitch_mm,dir=0,rot=0,ox=0,oy=0,visible=False):
                 my = (idx % y) * pitch_mm + ox
                 if (idx // x) % 2 == 1:
                     my = (y - (idx % y) - 1) * pitch_mm + ox
+                    if dir == 2 or dir == 3:
+                        rot += 180
+
                 mx = (idx // x) * pitch_mm + oy
-                if dir != 0:
+                if dir == 1 or dir == 3:
                     tx = mx
                     mx = my
                     my = tx
+
                 #print "idx = %s"%idx
                 #print "x = %d"%mx
                 #print "y = %d"%my
@@ -39,5 +52,8 @@ def arrange_leds(name,x,y,pitch_mm,dir=0,rot=0,ox=0,oy=0,visible=False):
 
     pcbnew.Refresh()
     
-#arrange_leds("SK6812",16,16,10,0)
-#arrange_leds("C",16,16,10,5,0,0,0)
+#arrange_leds("SK6812",10,10,10,0)
+#arrange_leds("C0603",10,10,10,0,5,0,0)
+
+#arrange_leds("SK6812MINI",16,16,6.25,2)
+#arrange_leds("C0603",16,16,6.25,2,180,5,0)
