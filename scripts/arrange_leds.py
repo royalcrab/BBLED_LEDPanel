d9f41595a19f74545af4a1540c22d8145ea67e2b
BBLED <sns@kabura-ya.net>
2021-03-12 01:18:45 +0900

    Update arrange_leds.py

--- scripts/arrange_leds.py
+++ scripts/arrange_leds.py
@@ -22,38 +22,42 @@ def arrange_leds(name,x,y,pitch_mm,dir=0,rot=0,ox=0,oy=0,visible=False):
     mods = board.GetModules()
 
     for mod in mods:
+        r = rot
         if mod.GetValue() == name:
             ref = mod.GetReference()
             res = re.match(r'\D+(\d+)',ref)
             #print ref
             if res:
                 idx = int(res.group(1))-1
-                my = (idx % y) * pitch_mm + ox
+                my = (idx % y) * pitch_mm
                 if (idx // x) % 2 == 1:
-                    my = (y - (idx % y) - 1) * pitch_mm + ox
-                    if dir == 2 or dir == 3:
-                        rot += 180
+                    my = (y - (idx % y) - 1) * pitch_mm
+                else:
+                    if (dir == 2 or dir == 3):
+                        r += 180
 
-                mx = (idx // x) * pitch_mm + oy
+                mx = (idx // x) * pitch_mm
                 if dir == 1 or dir == 3:
                     tx = mx
                     mx = my
                     my = tx
 
+                mx += ox
+                my += oy
+
                 #print "idx = %s"%idx
                 #print "x = %d"%mx
                 #print "y = %d"%my
                 mod.SetPosition(pcbnew.wxPointMM(mx,my))
-                mod.SetOrientation(rot*10)
+                mod.SetOrientation(r*10)
 
                 if visible == False:
                     silk = mod.Reference()
                     silk.SetVisible(False)
 
     pcbnew.Refresh()
-    
-#arrange_leds("SK6812",10,10,10,0)
-#arrange_leds("C0603",10,10,10,0,5,0,0)
+   
 
-#arrange_leds("SK6812MINI",16,16,6.25,2)
-#arrange_leds("C0603",16,16,6.25,2,180,5,0)
+#arrange_leds("BBLED_SK6812MINI",16,16,6.25,3)
+#arrange_leds("0.1uF",16,16,6.25,3,180,0,3)
+
